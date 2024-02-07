2a8e343b7759561bb65a884cff1ca849f93215f5
Prakarsh Verma <prakarsh1248@gmail.com>
2024-02-07 21:44:42 +0530

    Version 1

---
+++ routes/admin.js
@@ -0,0 +1,59 @@
+const { Router } = require("express");
+const adminMiddleware = require("../middleware/admin");
+const {Admin, Course} = require('../db/index');
+const router = Router();
+
+// Admin Routes
+router.post('/signup', (req, res) => {
+    // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //check if a admin with this username already exists
+    Admin.create({
+        username: username,
+        password: password
+    })
+    .then(function(){
+        res.json({
+            message: 'Admin created successfully'
+        })
+    })
+    .catch(function(){
+        res.json({
+            message: 'Admin was not created successfully'
+        })
+    })
+});
+
+router.post('/courses', adminMiddleware, async(req, res) => {
+    // Implement course creation logic
+    // const username = req.headers.username;
+    // const password = req.headers.password;
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+    //zod can be used for input validation
+    const newCourse = await Course.create({
+        title: title,
+        description: description,
+        imageLink: imageLink,
+        price: price,
+    })
+    res.json({
+        message: 'Course created successfully',
+        courseId: newCourse._id
+    })
+    
+});
+
+router.get('/courses', adminMiddleware, async(req, res) => {
+    // Implement fetching all courses logic
+    const allCourses = await Course.find({})
+    res.json({
+        courses: allCourses
+    }) 
+});
+
+module.exports = router;
